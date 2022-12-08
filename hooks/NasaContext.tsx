4263313164789fd0a74d646d2b5216622f390e7a
Raissa K <raissakuzerdev@gmail.com>
2022-12-08 04:51:13 -0300

    Removes unnecessary function

--- hooks/NasaContext.tsx
+++ hooks/NasaContext.tsx
@@ -46,7 +46,6 @@ export default function NasaProvider({ children }: NasaProviderPropTypes) {
 		const convertedDate = {
 			date: userDate
 		}
-		console.log(userDate, convertedDate)
 		setDate(convertedDate);
 	};
 
